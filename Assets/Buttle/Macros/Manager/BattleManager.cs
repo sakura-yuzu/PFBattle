8de3940288e8c02294cccc8ba93b22133030af93
sakura-yuzu <katakuripowder@gmail.com>
2025-09-26 20:16:59 +0900

    敵味方の生成を別クラスに射出

--- Assets/Buttle/Macros/Manager/BattleManager.cs
+++ Assets/Buttle/Macros/Manager/BattleManager.cs
@@ -29,17 +29,10 @@ class BattleManager : MonoBehaviour
 	public GameObject characterStatusPanel;
 
 	private ActionManager actionManager;
-
-	private List<Vector3> enemyPositionList;
+	private BattleStageManager battleStageManager;
 
 	private bool BattleContinue = true;
 	private CancellationToken cancellationToken;
-
-	public List<Vector3> LoadFromJSON(string json)
-	{
-		var data = JsonUtility.FromJson<EnemyPositionData>(json);
-		return data.enemyPositions;
-	}
 
 	async void Awake()
 	{
@@ -60,54 +53,17 @@ class BattleManager : MonoBehaviour
 			selectTargetAllyPanel,
 			selectTargetEnemyPanel
 		);
-		// ステージ生成
-		Instantiate(battleData.stage.field, new Vector3(0, 0, 0), Quaternion.identity);
 
-		// エネミーの位置を読み込み
-		string json = File.ReadAllText(battleData.stage.filePath);
-		enemyPositionList = LoadFromJSON(json);
+		battleStageManager = new BattleStageManager(battleData, selectTargetEnemyPanel, selectTargetAllyPanel);
+		await battleStageManager.Prepare();
 
-		int i = 0;
-		// エネミー生成
-		foreach (CreatureSetting enemySetting in battleData.stage.enemyList)
-		{
-			Creature enemyComponent = await instantiateCharacter(enemySetting, enemyPositionList[i++], selectTargetEnemyPanel);
-			enemies.Add(enemyComponent);
-		}
-
-		foreach (CreatureSetting characterSetting in battleData.selectedCharacterList)
-		{
-			Creature allyComponent = await instantiateCharacter(characterSetting, new Vector3(0, 0, 0), selectTargetAllyPanel);
-			allies.Add(allyComponent);
-		}
+		enemies = battleStageManager.enemies;
+		allies = battleStageManager.allies;
 		selectTargetAllyPanel.GetComponent<SelectTargetAllyPanel>().setAllies(allies);
 		await selectTargetAllyPanel.GetComponent<SelectTargetAllyPanel>().Prepare();
 		selectTargetEnemyPanel.GetComponent<SelectTargetEnemyPanel>().setEnemies(enemies);
 		await selectTargetEnemyPanel.GetComponent<SelectTargetEnemyPanel>().Prepare();
 	}
-
-	private async UniTask<Creature> instantiateCharacter(CreatureSetting creatureSetting, Vector3 position, GameObject parent)
-	{
-		var characterPrefab = await Addressables.LoadAssetAsync<GameObject>(creatureSetting.prefabAddress).Task;
-		GameObject characterObject = Instantiate(characterPrefab);
-		characterObject.transform.position = position;
-		Creature characterComponent = characterObject.GetComponent<Creature>();
-
-		// 同名のキャラクターが存在する場合、接尾辞を付けて区別する
-		string baseName = creatureSetting.displayName;
-		string uniqueName = baseName;
-		int suffix = 1;
-
-		while (enemies.Any(e => e.displayName == uniqueName) || allies.Any(a => a.displayName == uniqueName))
-		{
-			uniqueName = baseName + suffix.ToString();
-			suffix++;
-		}
-
-		characterComponent.displayName = uniqueName;
-		return characterComponent;
-	}
-
 	private async UniTask Battle()
 	{
 		cancellationToken = this.GetCancellationTokenOnDestroy();
@@ -158,10 +114,5 @@ class BattleManager : MonoBehaviour
 	private void SceneExit()
 	{
 		SceneManager.LoadScene("ResultScene");
-	}
-
-	public class EnemyPositionData
-	{
-		public List<Vector3> enemyPositions;
 	}
 }
